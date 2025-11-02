e0655a9cda28bcf185c1a879996e1a665eac2776
SofiaRSantana <sofia.santana@germinare.org.br>
2025-11-01 21:38:20 -0300

    corrigindo a url da api do chatbot

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
 
-response = requests.post(url='http://172.31.28.40:8000/programs_agent', json={"prompt" : "O que é a lei 8.171/1991?"})
+response = requests.post(url='http://18.207.93.176:8000/search_agent', json={"prompt" : "O que é a lei 8.171/1991?"})
 
 print(response.text)
